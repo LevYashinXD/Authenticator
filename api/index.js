8292404025500923be6db5378d3d34fa1fd4c948
LevYashinXD <johngreentamem@gmail.com>
2025-08-21 21:52:05 -0500

    done

--- api/index.js
+++ api/index.js
@@ -1,11 +1,5 @@
 // File: /api/index.js
-
-// This function will be run by Vercel every time someone visits your URL.
 export default function handler(request, response) {
-  
-  // THIS IS THE ONLY LINE YOU WILL EVER EDIT
-  const ngrokUrl = 'https://YOUR_CURRENT_NGROK_URL_HERE.ngrok-free.app';
-
-  // Send the URL back as plain text
+  const ngrokUrl = 'https://b81a54d880ad.ngrok-free.app';
   response.status(200).send(ngrokUrl);
 }
